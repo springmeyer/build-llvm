06d8eda3ef78085bde959afd39d5252609a50762
Dane Springmeyer <dane@mapbox.com>
2015-03-24 21:10:54 -0700

    various build fixes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 PREFIX=${PREFIX:-"/opt/llvm/"}
 
-function abort { >&2 echo -e "\033[1m\033[31m$1\033[0m"; exit 1; }
+function abort { >&2 echo -e "\033[1m\033[31m$1\033[0m"; exit 0; }
 
 function setup_cpp11() {
     if [[ ! -d ./.mason ]]; then
@@ -19,30 +19,45 @@ function setup_cpp11() {
     export PATH=$(pwd)/.mason:$PATH
 }
 
+function build() {
+    # TODO - get this working or embed custom libc++
+    #CLIB=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include:/usr/include
+    #CPPLIB=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
+    #  --with-c-include-dirs=${CLIB}:${CPPLIB}
+    OPTS=""
+    if [[ $(uname -s) == 'Darwin' ]]; then
+        OPTS="--enable-libcpp --enable-cxx11"
+    fi
+    ./configure --prefix=${PREFIX} --enable-optimized --enable-clang-static-analyzer --disable-assertions $OPTS
+    time make ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 -j${JOBS}
+    make install ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 -j${JOBS}
+    cp Release/bin/clang "${PREFIX}/bin/clang"
+    strip -x ${PREFIX}/bin/clang
+
+}
+
 function setup() {
-    git clone --depth 1 http://llvm.org/git/llvm.git
-    cd llvm/tools
-    git clone --depth 1 http://llvm.org/git/clang.git
-    git clone --depth 1 http://llvm.org/git/lldb.git
-    cd clang/tools
-    git clone --depth 1 http://llvm.org/git/clang-tools-extra.git
-    svn co http://include-what-you-use.googlecode.com/svn/trunk/ include-what-you-use
-    perl -p -i -e 's/diagtool/diagtool include-what-you-use/g' Makefile
-    cd ../../../
-    cd ./projects
-    git clone --depth 1 http://llvm.org/git/compiler-rt.git
-    cd ../
-    git config branch.master.rebase true
-    ./configure --prefix=${PREFIX} --enable-clang-static-analyzer --enable-optimized
-    make && make install
+    if [[ ! -d llvm ]]; then
+        git clone --depth 1 http://llvm.org/git/llvm.git
+        cd llvm/tools
+        git clone --depth 1 http://llvm.org/git/clang.git
+        # c-index-test build is broken against libxml2
+        perl -p -i -e 's/c-index-test//g' clang/tools/Makefile
+        git clone --depth 1 http://llvm.org/git/lldb.git
+        cd clang/tools
+        git clone --depth 1 http://llvm.org/git/clang-tools-extra.git extra
+        svn co http://include-what-you-use.googlecode.com/svn/trunk/ include-what-you-use
+        perl -p -i -e 's/diagtool/diagtool include-what-you-use/g' Makefile
+        cd ../../../
+        cd ./projects
+        git clone --depth 1 http://llvm.org/git/compiler-rt.git
+        cd ../
+        git config branch.master.rebase true
+    fi
 }
 
 function update() {
     CLEAN=""
-    if [[ -d ${PREFIX} ]]; then
-        echo "Please remove $PREFIX before continuing"
-        exit 1
-    fi
     cd llvm
     ${CLEAN} && git pull
     cd tools/lldb
@@ -57,19 +72,6 @@ function update() {
     cd ../../../../projects/compiler-rt/
     ${CLEAN} && git pull
     cd ../../
-    # TODO - get this working or embed custom libc++
-    #CLIB=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include:/usr/include
-    #CPPLIB=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
-    #  --with-c-include-dirs=${CLIB}:${CPPLIB}
-    OPTS=""
-    if [[ $(uname -s) == 'Darwin' ]]; then
-        OPTS="--enable-libcpp --enable-cxx11"
-    fi
-    ./configure --prefix=${PREFIX} --enable-optimized --enable-clang-static-analyzer --disable-assertions $OPTS
-    time make ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 -j${JOBS}
-    make install ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 -j${JOBS}
-    cp Release/bin/clang "${PREFIX}/bin/clang"
-    strip -x ${PREFIX}/bin/clang
 }
 
 function main() {
@@ -78,8 +80,14 @@ function main() {
     if [[ ! -d llvm ]]; then
         setup
     else
+        if [[ -d ${PREFIX} ]]; then
+            echo
+            echo "Warning: installing over existing ${PREFIX}"
+            echo
+        fi
         update
     fi
+    build
 }
 
 main
