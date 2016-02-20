6dac74c04e0d5f35145f3716f8846329f4092eb8
Dane Springmeyer <dane@mapbox.com>
2016-02-20 12:07:16 -0800

    fix default c++ include paths on OS X + switch to cmake

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e -u
 set -o pipefail
 
 PREFIX=${PREFIX:-"/opt/llvm/"}
-JOBS=${JOBS:-4}
+JOBS=${JOBS:-8}
 CWD=$(pwd)
 
 # lldb has been frequently not compiling, so I've given up enabling it by default
@@ -12,26 +12,35 @@ ENABLE_LLDB=false
 
 function abort { >&2 echo -e "\033[1m\033[31m$1\033[0m"; exit 0; }
 
-
 function build() {
     # TODO - get this working or embed custom libc++
     #CLIB=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/6.0/include:/usr/include
     #CPPLIB=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
-    #  --with-c-include-dirs=${CLIB}:${CPPLIB}
-    OPTS=""
-    if [[ $(uname -s) == 'Darwin' ]]; then
-        OPTS="--enable-libcpp --enable-cxx11"
-    fi
     cd $CWD
-    rm -rf ./build
-    mkdir ./build
+    #rm -rf ./build
+    #mkdir ./build
     cd ./build
-    ../llvm/configure --prefix=${PREFIX} --enable-optimized --enable-clang-static-analyzer --disable-assertions $OPTS
-    time make ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 -j${JOBS}
-    make install ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 -j${JOBS}
-    cp Release/bin/clang "${PREFIX}/bin/clang"
-    strip -x ${PREFIX}/bin/clang
-
+    # does not have cstdint
+    #-DC_INCLUDE_DIRS=:/usr/include/c++/4.2.1/ \
+    # fail
+    # -DC_INCLUDE_DIRS=:../../../../../Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/ \
+    # -DDEFAULT_SYSROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk \
+    # NOTE: the C_INCLUDE_DIRS are appended to the DEFAULT_SYSROOT
+    # the DEFAULT_SYSROOT should be:
+    # Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk
+    # but then it is hard to append and have found the C includes in /usr/include
+    # and the C++ includes in /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/
+    # so we fake the sysroot to be / to make it easy to append those specific paths
+    cmake ../llvm -G Ninja -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+     -DCLANG_DEFAULT_CXX_STDLIB=libc++ \
+     -DC_INCLUDE_DIRS=:/usr/include:/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1/ \
+     -DDEFAULT_SYSROOT=/ \
+     -DCLANG_VENDOR=mapbox/springmeyer \
+     -DCLANG_REPOSITORY_STRING=https://github.com/springmeyer/build-llvm \
+     -DCLANG_APPEND_VC_REV=$(git -C ../llvm/tools/clang/ rev-list --max-count=1 HEAD) \
+     -DCLANG_VENDOR_UTI=org.mapbox.clang
+    ninja -j${JOBS}
+    ninja install
 }
 
 function setup() {
@@ -39,40 +48,36 @@ function setup() {
         git clone --depth 1 http://llvm.org/git/llvm.git
         cd llvm/tools
         git clone --depth 1 http://llvm.org/git/clang.git
-        # c-index-test build is broken against libxml2
-        perl -p -i -e 's/c-index-test//g' clang/tools/Makefile
         if [[ ${ENABLE_LLDB} == true ]]; then
             git clone --depth 1 http://llvm.org/git/lldb.git
         fi
         cd clang/tools
         git clone --depth 1 http://llvm.org/git/clang-tools-extra.git extra
-        svn co http://include-what-you-use.googlecode.com/svn/trunk/ include-what-you-use
-        perl -p -i -e 's/diagtool/diagtool include-what-you-use/g' Makefile
+        git clone --depth 1 git@github.com:include-what-you-use/include-what-you-use.git
         cd ../../../
         cd ./projects
         git clone --depth 1 http://llvm.org/git/compiler-rt.git
-        #(cd compiler-rt && patch -N < ${CWD}/compiler-rt.diff)
         cd ../
-        git config branch.master.rebase true
     fi
 }
 
 function update() {
     echo "**** updating llvm"
     cd llvm
-    git pull
+    GIT_PULL="git pull --rebase"
     if [[ ${ENABLE_LLDB} == true ]]; then
-        (echo "**** updating lldb" && cd tools/lldb && git pull)
+        (echo "**** updating lldb" && cd tools/lldb && ${GIT_PULL})
     fi
-    (echo "**** updating clang" && cd tools/clang && git pull)
-    (echo "**** updating clang-tools-extra" && cd tools/clang/tools/extra && git pull)
-    (echo "**** updating include-what-you-use" && cd tools/clang/tools/include-what-you-use && svn up)
-    (echo "**** updating compiler-rt" && cd projects/compiler-rt && git pull)
+    (echo "**** updating clang" && cd tools/clang && ${GIT_PULL})
+    (echo "**** updating clang-tools-extra" && cd tools/clang/tools/extra && ${GIT_PULL})
+    (echo "**** updating include-what-you-use" && cd tools/clang/tools/include-what-you-use && ${GIT_PULL})
+    (echo "**** updating compiler-rt" && cd projects/compiler-rt && ${GIT_PULL})
 }
 
 function main() {
     which git || abort 'please install git'
-    which svn || abort 'please install svn'
+    which cmake || abort 'please install cmake'
+    which ninja || abort 'please install ninja'
     # designed to limit the need for extra deps to build lldb
     if [[ ${ENABLE_LLDB} == true ]]; then
         export CXXFLAGS="-DLLDB_DISABLE_PYTHON -DLLDB_DISABLE_CURSES -DLLDB_DISABLE_LIBEDIT -DLLVM_ENABLE_TERMINFO=0"
